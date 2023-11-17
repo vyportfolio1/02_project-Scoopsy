fc3f14f3dc3172abf647449886bff2802383dde9
vyportfolio1 <vyportfolio1@gmail.com>
2023-11-17 16:27:34 +0200

    slides

--- index.js
+++ index.js
@@ -1,31 +1,53 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const carousel = document.querySelector(".all-carusel");
-    const prevBtn = document.querySelector(".fa-chevron-left");
-    const nextBtn = document.querySelector(".fa-chevron-right");
+let currentIndex = 0;
+const items = document.querySelectorAll('.ice');
+const totalItems = items.length;
+const itemWidth = items[0].offsetWidth;
 
-    let currentIndex = 0;
+function nextImage() {
+  currentIndex = (currentIndex + 1) % totalItems;
+  updateCarousel();
+}
 
-    function showSlide(index) {
-        const slides = document.querySelectorAll(".ice");
-        slides.forEach((slide, i) => {
-            slide.style.display = i === index ? "block" : "none";
+function prevImage() {
+  currentIndex = (currentIndex - 1 + totalItems) % totalItems;
+  updateCarousel();
+}
+
+function updateCarousel() {
+  const newTransformValue = -currentIndex * itemWidth + 'px';
+  items.forEach(item => {
+    item.style.transform = `translateX(${newTransformValue})`;
+    item.style.transition = 'transform 1s ease';
+  });
+
+  // Check if it's the last transitioned item and reset the position
+  items.forEach((item, index) => {
+    item.addEventListener('transitionend', function () {
+      if (index === totalItems - 1 && currentIndex === 0) {
+        items.forEach(item => {
+          item.style.transition = 'none';
+          item.style.transform = `translateX(${-totalItems * itemWidth}px)`;
         });
-    }
 
-    function nextSlide() {
-        currentIndex = (currentIndex + 1) % carousel.children.length;
-        showSlide(currentIndex);
-    }
+        // Adding a delay to reset the transition property after the reset
+        setTimeout(() => {
+          items.forEach(item => {
+            item.style.transition = 'transform 1s ease';
+          });
+        }, 50);
+      } else if (index === 0 && currentIndex === totalItems - 1) {
+        items.forEach(item => {
+          item.style.transition = 'none';
+          item.style.transform = 'translateX(0)';
+        });
 
-    function prevSlide() {
-        currentIndex = (currentIndex - 1 + carousel.children.length) % carousel.children.length;
-        showSlide(currentIndex);
-    }
-
-    // Show the initial slide
-    showSlide(currentIndex);
-
-    // Add event listeners to arrow buttons
-    nextBtn.addEventListener("click", nextSlide);
-    prevBtn.addEventListener("click", prevSlide);
-});
+        // Adding a delay to reset the transition property after the reset
+        setTimeout(() => {
+          items.forEach(item => {
+            item.style.transition = 'transform 1s ease';
+          });
+        }, 50);
+      }
+    });
+  });
+}
