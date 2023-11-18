75f1b8cb6c19d53eb207af57a017fde698a5e314
vyportfolio1 <vyportfolio1@gmail.com>
2023-11-18 11:52:40 +0200

    mobile

--- index.js
+++ index.js
@@ -1,53 +1,34 @@
+const sliderContainer = document.getElementById('imageSlider');
+const images = sliderContainer.querySelectorAll('.ice');
+
+ 
 let currentIndex = 0;
-const items = document.querySelectorAll('.ice');
-const totalItems = items.length;
-const itemWidth = items[0].offsetWidth;
 
+ 
+function showImage(index) {
+  
+  images.forEach((image, i) => {
+      image.style.transform = `translateX(${(i - index) * 70}%)`;
+      image.style.transition = "1s ease";
+  });
+  currentIndex = index;
+}
+
+ 
 function nextImage() {
-  currentIndex = (currentIndex + 1) % totalItems;
-  updateCarousel();
+  currentIndex = (currentIndex + 1) % images.length;
+  showImage(currentIndex);
 }
 
+ 
 function prevImage() {
-  currentIndex = (currentIndex - 1 + totalItems) % totalItems;
-  updateCarousel();
+  currentIndex = (currentIndex - 1 + images.length) % images.length;
+  showImage(currentIndex);
 }
 
-function updateCarousel() {
-  const newTransformValue = -currentIndex * itemWidth + 'px';
-  items.forEach(item => {
-    item.style.transform = `translateX(${newTransformValue})`;
-    item.style.transition = 'transform 1s ease';
-  });
+ 
+document.querySelector('.leftb').addEventListener('click', prevImage);
+document.querySelector('.rightb').addEventListener('click', nextImage);
 
-  // Check if it's the last transitioned item and reset the position
-  items.forEach((item, index) => {
-    item.addEventListener('transitionend', function () {
-      if (index === totalItems - 1 && currentIndex === 0) {
-        items.forEach(item => {
-          item.style.transition = 'none';
-          item.style.transform = `translateX(${-totalItems * itemWidth}px)`;
-        });
-
-        // Adding a delay to reset the transition property after the reset
-        setTimeout(() => {
-          items.forEach(item => {
-            item.style.transition = 'transform 1s ease';
-          });
-        }, 50);
-      } else if (index === 0 && currentIndex === totalItems - 1) {
-        items.forEach(item => {
-          item.style.transition = 'none';
-          item.style.transform = 'translateX(0)';
-        });
-
-        // Adding a delay to reset the transition property after the reset
-        setTimeout(() => {
-          items.forEach(item => {
-            item.style.transition = 'transform 1s ease';
-          });
-        }, 50);
-      }
-    });
-  });
-}
+ 
+showImage(currentIndex);
