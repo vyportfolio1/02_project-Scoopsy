cdfa95c87d14cafcabad6024751da65abcdd5b6c
vyportfolio1 <vyportfolio1@gmail.com>
2023-11-18 14:56:33 +0200

    moving foward

--- index.js
+++ index.js
@@ -1,34 +1,59 @@
-const sliderContainer = document.getElementById('imageSlider');
-const images = sliderContainer.querySelectorAll('.ice');
 
- 
-let currentIndex = 0;
+  const specialsSlider = document.getElementById('imageSlider');
+  const specialImages = specialsSlider.querySelectorAll('.ice');
+  let currentSpecialIndex = 0;
 
- 
-function showImage(index) {
-  
-  images.forEach((image, i) => {
-      image.style.transform = `translateX(${(i - index) * 70}%)`;
-      image.style.transition = "1s ease";
+  function showSpecial(index) {
+    specialImages.forEach((image, i) => {
+      const width = image.offsetWidth;
+      const translateValue = (i - index) * width;
+      image.style.transform = `translateX(${translateValue}px)`;
+      image.style.transition = '1s ease';
+    });
+    currentSpecialIndex = index;
+  }
+
+  function nextSpecial() {
+    currentSpecialIndex = (currentSpecialIndex + 1) % specialImages.length;
+    showSpecial(currentSpecialIndex);
+  }
+
+  function prevSpecial() {
+    currentSpecialIndex = (currentSpecialIndex - 1 + specialImages.length) % specialImages.length;
+    showSpecial(currentSpecialIndex);
+  }
+
+  document.querySelector('.leftb').addEventListener('click', prevSpecial);
+  document.querySelector('.rightb').addEventListener('click', nextSpecial);
+
+  showSpecial(currentSpecialIndex);
+
+
+const flavorSlider = document.getElementById('flavorSlider');
+const flavorImages = flavorSlider.querySelectorAll('.ice2');
+let currentFlavorIndex = 0;
+
+function showFlavor(index) {
+  flavorImages.forEach((image, i) => {
+    const width = image.offsetWidth;
+    const translateValue = (i - index) * width;
+    image.style.transform = `translateX(calc(50% + ${translateValue}px))`;
+    image.style.transition = '1s ease';
   });
-  currentIndex = index;
+  currentFlavorIndex = index;
 }
 
- 
-function nextImage() {
-  currentIndex = (currentIndex + 1) % images.length;
-  showImage(currentIndex);
+function nextFlavor() {
+  currentFlavorIndex = (currentFlavorIndex + 1) % flavorImages.length;
+  showFlavor(currentFlavorIndex);
 }
 
- 
-function prevImage() {
-  currentIndex = (currentIndex - 1 + images.length) % images.length;
-  showImage(currentIndex);
+function prevFlavor() {
+  currentFlavorIndex = (currentFlavorIndex - 1 + flavorImages.length) % flavorImages.length;
+  showFlavor(currentFlavorIndex);
 }
 
- 
-document.querySelector('.leftb').addEventListener('click', prevImage);
-document.querySelector('.rightb').addEventListener('click', nextImage);
+document.querySelector('.leftf').addEventListener('click', prevFlavor);
+document.querySelector('.rightf').addEventListener('click', nextFlavor);
 
- 
-showImage(currentIndex);
+showFlavor(currentFlavorIndex);
