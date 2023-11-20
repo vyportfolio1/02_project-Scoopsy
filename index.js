d49fa5986efd0f0cf63201c3d2a2d94c11766b30
vyportfolio1 <vyportfolio1@gmail.com>
2023-11-20 16:44:21 +0200

    burger meniu

--- index.js
+++ index.js
@@ -1,32 +1,38 @@
+const imageSlider = document.getElementById('imageSlider');
+const iceImages = imageSlider.querySelectorAll('.ice');
+let currentImageIndex = 0;
 
-  const specialsSlider = document.getElementById('imageSlider');
-  const specialImages = specialsSlider.querySelectorAll('.ice');
-  let currentSpecialIndex = 0;
+function showImage(index) {
+  const containerWidth = imageSlider.offsetWidth;
+  const numImages = iceImages.length;
+  const imageWidth = containerWidth / numImages;
 
-  function showSpecial(index) {
-    specialImages.forEach((image, i) => {
-      const width = image.offsetWidth;
-      const translateValue = (i - index) * width;
-      image.style.transform = `translateX(${translateValue}px)`;
-      image.style.transition = '1s ease';
-    });
-    currentSpecialIndex = index;
-  }
+  iceImages.forEach((image, i) => {
+    const translateValue = (i - index) * imageWidth;
+    image.style.transform = `translateX(calc(50% + ${translateValue}px))`;
+    image.style.transition = '1s ease';
+  });
+  currentImageIndex = index;
+}
 
-  function nextSpecial() {
-    currentSpecialIndex = (currentSpecialIndex + 1) % specialImages.length;
-    showSpecial(currentSpecialIndex);
-  }
+function nextImage() {
+  currentImageIndex = (currentImageIndex + 1) % iceImages.length;
+  showImage(currentImageIndex);
+}
 
-  function prevSpecial() {
-    currentSpecialIndex = (currentSpecialIndex - 1 + specialImages.length) % specialImages.length;
-    showSpecial(currentSpecialIndex);
-  }
+function prevImage() {
+  currentImageIndex = (currentImageIndex - 1 + iceImages.length) % iceImages.length;
+  showImage(currentImageIndex);
+}
 
-  document.querySelector('.leftb').addEventListener('click', prevSpecial);
-  document.querySelector('.rightb').addEventListener('click', nextSpecial);
+window.addEventListener('resize', () => {
+  showImage(currentImageIndex);
+});
 
-  showSpecial(currentSpecialIndex);
+document.querySelector('.leftb').addEventListener('click', prevImage);
+document.querySelector('.rightb').addEventListener('click', nextImage);
+
+showImage(currentImageIndex);
 
 
 const flavorSlider = document.getElementById('flavorSlider');
@@ -36,7 +42,7 @@ let currentFlavorIndex = 0;
 function showFlavor(index) {
   flavorImages.forEach((image, i) => {
     const width = image.offsetWidth;
-    const translateValue = (i - index) * width;
+    const translateValue = (i - index) * (width * 0.5);  
     image.style.transform = `translateX(calc(50% + ${translateValue}px))`;
     image.style.transition = '1s ease';
   });
@@ -57,3 +63,17 @@ document.querySelector('.leftf').addEventListener('click', prevFlavor);
 document.querySelector('.rightf').addEventListener('click', nextFlavor);
 
 showFlavor(currentFlavorIndex);
+
+const meniuIcon = document.querySelector(".fa-xmarks-lines")
+const meniuClose = document.querySelector(".fa-circle-xmark")
+const hiddenMeniu = document.querySelector(".mm")
+
+meniuIcon.addEventListener("click", ()=>{
+  hiddenMeniu.classList.remove("hidden");
+  hiddenMeniu.style.transition = "opacity 1s ease-in"
+})
+
+meniuClose.addEventListener("click", ()=>{
+  hiddenMeniu.classList.add("hidden")
+  hiddenMeniu.style.transition = "opacity 1s ease-in"
+})
